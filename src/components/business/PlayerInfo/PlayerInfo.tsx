7e162582a74bf64365b36caa440032ce3e35e91d
Antoine Blanco <antoine.blanco@etu.univ-poitiers.fr>
2024-07-21 18:32:58 +0200

    ✨ Add average to Player Info

--- src/components/business/PlayerInfo/PlayerInfo.tsx
+++ src/components/business/PlayerInfo/PlayerInfo.tsx
@@ -1,12 +1,16 @@
 import { PlayerInfo as PlayerInfoModel } from "../../../models/player";
+import { Rating as RatingModel, RatingType } from "../../../models/rating";
+import Rating from "../../ui/Rating/Rating";
 import StyledDetails from "../../ui/StyledDetails/StyledDetails";
 
-export type PlayerInfoProps = { info: PlayerInfoModel };
+export type PlayerInfoProps = { info: PlayerInfoModel; average: RatingModel };
 
-export default function PlayerInfo({ info }: PlayerInfoProps) {
+export default function PlayerInfo({ info, average }: PlayerInfoProps) {
   return (
     <StyledDetails>
-      <StyledDetails.Summary>{info.name}</StyledDetails.Summary>
+      <StyledDetails.Summary>
+        {info.name} <Rating type={RatingType.Stars} value={average.value} />
+      </StyledDetails.Summary>
       <StyledDetails.Body>
         <ul>
           {Object.keys(info).map((key) => (
